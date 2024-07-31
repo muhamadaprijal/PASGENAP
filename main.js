a3230104e37f67bb6956834e90c63b8e31889712
https://github.com/muhamadaprijal/Produk <aprijalmaprijal32@gmail.com>
2024-07-31 09:40:57 +0700

    update

--- main.js
+++ main.js
@@ -11,37 +11,35 @@ import {
   orderBy,
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
-  authDomain: "aprijal",
-  firebaseapp.com"aprijal,
-  projectId: "aprijal",
-  storageBucket: "aprijal.appspot.com",
-  messagingSenderId: "1094982396668",
-  appId: "1:1094982396668:web:2d103526ab40a59efc0579",
-  measurementId: "G-MF48P7VG5P"
+  apiKey: "AIzaSyDO3zW_rQohD-ktTXkrrrILsV_e1-HjB5w",
+  authDomain: "insan-cemerlang-8b943.firebaseapp.com",
+  projectId: "insan-cemerlang-8b943",
+  storageBucket: "insan-cemerlang-8b943.appspot.com",
+  messagingSenderId: "1037716880990",
+  appId: "1:1037716880990:web:5bb29cf4b27b53ccff7ecf",
+  measurementId: "G-ZHPLRWYEQB"
 };
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function ambilDaftarAbsensi() {
-  const refDokumen = collection(db, "absensi");
-  const kueri = query(refDokumen, orderBy("nama"));
+  const refDokumen = collection(db, "apsensi");
+  const kueri = query(refDokumen, orderBy("Nama"));
   const cuplikanKueri = await getDocs(kueri);
 
   let hasil = [];
   cuplikanKueri.forEach((dok) => {
     hasil.push({
       id: dok.id,
-      tanggal: dok.data().tanggal,
-      nis: dok.data().nis,
-      nama: dok.data().nama,
-      alamat: dok.data().alamat,
-      noTlpn: dok.data().noTlpn,
-      kelas: dok.data().kelas,
-      keterangan: dok.data().keterangan,
+      Tanggal: dok.data().Tanggal,
+      Nis: dok.data().Nis,
+      Nama: dok.data().Nama,
+      Alamat: dok.data().Alamat,
+      NoTlpn: dok.data().NoTlpn,
+      Kelas: dok.data().Kelas,
+      Keterangan: dok.data().Keterangan,
       
     });
   });
@@ -52,19 +50,19 @@ export async function ambilDaftarAbsensi() {
 }
 
 export function formatAngka(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "apsensi");
 }
 
 export async function tambahAbsensi(tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
   try {
-    const dokRef = await addDoc(collection(db, 'absensi'), {
-      tanggal: tanggal,
-      nis: nis,
-      nama: nama,
-      alamat: alamat,
-      noTlpn: noTlpn,
-      kelas: kelas,
-      keterangan: keterangan
+    const dokRef = await addDoc(collection(db, 'apsensi'), {
+      Tanggal: Tanggal,
+      Nis: Nodeis,
+      Nama: Nama,
+      Alamat: Alamat,
+      NoTlpn: NoTlpn,
+      Kelas: Kelas,
+      Keterangan: Keterangan
     });
     console.log('berhasil menembah ' + dokRef.id);
   } catch (e) {
@@ -73,11 +71,11 @@ export async function tambahAbsensi(tanggal, nis, nama, alamat, noTlpn, kelas, k
 }
 
 export async function hapusAbsensi(docId) {
-  await deleteDoc(doc(db, "absensi", docId));
+  await deleteDoc(doc(db, "apsensi", docId));
 }
 
 export async function ubahPembeli(docId, nama, alamat, noTlpn) {
-  await updateDoc(doc(db, "pembeli", docId), {
+  await updateDoc(doc(db, "apsensi", docId), {
     nama: nama,
     alamat: alamat,
     noTlpn: noTlpn
@@ -85,7 +83,7 @@ export async function ubahPembeli(docId, nama, alamat, noTlpn) {
 }
 
 export async function ambilPembeli(docId) {
-  const docRef = await doc(db, "pembeli", docId);
+  const docRef = await doc(db, "apsensi", docId);
   const docSnap = await getDoc(docRef);
 
   return await docSnap.data();
